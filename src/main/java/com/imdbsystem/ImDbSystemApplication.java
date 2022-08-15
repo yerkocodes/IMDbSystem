4a4b3a74e87f66d590980f17f0fc23f4fbbbdde8
Yerko Cortes Baeza <yerkodigo@gmail.com>
2022-08-15 14:39:55 -0400

    First Commit

---
+++ src/main/java/com/imdbsystem/ImDbSystemApplication.java
@@ -0,0 +1,13 @@
+package com.imdbsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImDbSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImDbSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imdbsystem/ImDbSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imdbsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImDbSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
